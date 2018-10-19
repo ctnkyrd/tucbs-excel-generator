71f4ebdcb31c0edc34b7f7415c13e662e580e4b0
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-10-19 23:02:19 +0300

    connection correct

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import psycopg2 as pg, datetime
-from pgget import Connection as c
+from pgget import Connection
 from kurum import Kurum
 
-cs = "dbname=%s user=%s password=%s host=%s port=%s" % ('tucbsdata','postgres','Ankara123','192.168.30.136','5432')
-conn = pg.connect(cs)
-cur = conn.cursor()
-cur.execute("select * from kurum where analiz_tamamlandi_first is true")
-kurum = cur.fetchall()
+cnn = Connection()
+
+kurum = cnn.getlistofdata('kurum','*','analiz_tamamlandi_first is true')
 
 for i in kurum:
     a = Kurum(i[0], i[3], i[8], i[2], i[1])
--- pgget.py
+++ pgget.py
@@ -3,18 +3,18 @@ import psycopg2 as pg
 
 class Connection:
     def __init__(self):
-        self.cs = "dbname=%s user=%s password=%s host=%s port=%s" % ('tucbsdata','postgres','Ankara123','192.168.30.136','5432')
+        self.cs = "dbname=%s user=%s password=%s host=%s port=%s" % ('tucbsdata','postgres','kalman','localhost','5432')
 
-    def getsinglekoddata(tableName, valueColumn, where = ""):
+    def getsinglekoddata(self, tableName, valueColumn, where = ""):
         conn = pg.connect(self.cs)
         cur = conn.cursor()
         cur.execute("select %s from %s where %s" % (valueColumn, tableName, where)+" limit 1")
         data = cur.fetchone()
         return data[0]
 
-    def getlistofdata(tableName, columns='*', where=''):
+    def getlistofdata(self, tableName, columns='*', where=''):
         conn = pg.connect(self.cs)
         cur = conn.cursor()
-        cur.execute("select %s from %s where %s" % (tableName, columns, where))
+        cur.execute("select %s from %s where %s" % (columns,tableName, where))
         data = cur.fetchall()
         return data
