4d2f5c2d77db4e171ff29b738aaa21ff40f11089
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-10-22 11:45:57 +0300

    add folder structure

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ kurum = cnn.getlistofdata('kurum','objectid','analiz_tamamlandi_first is true')
 counter=0
 cvdict = dict_veri_katmani
 for i in kurum:
+    
     newKurum = Kurum(i[0])
-    print newKurum.bakanlik +"-->"+ newKurum.adi, 'Started!'
+    print datetime.datetime.now().strftime('[%Y-%m-%d][%H:%M:%S]'), unicode(newKurum.adi)
     ek2_oid = newKurum.ek2_oid
     
     kurumKatmanalri = cnn.getlistofdata('x_ek_2_tucbs_veri_katmani','*','geodurum is true and ek_2='+str(ek2_oid))
@@ -38,8 +39,8 @@ for i in kurum:
                             katman[cvdict['vk_tematik_nicel_yeni']], katman[cvdict['vk_tematik_nicel_olmayan_yeni']], katman[cvdict['vk_aciklama']])
         try:
             counter += 1
-            print str(counter) +"-->"+ cvf.katman_adi.decode('utf-8')
+            # print str(counter) +"-->"+ cvf.katman_adi.decode('utf-8')
             # excel created here
             cvf.createExcelFile()
         except BaseException as be:
-            print be.message
+            print be
